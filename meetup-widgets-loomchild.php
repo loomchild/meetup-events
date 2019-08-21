113f4ec225b11a4a2b7531dcb99d1ed91be9e91f
Jarek Lipski <loomchild@loomchild.net>
2019-08-21 20:14:03 +0200

    add main plugin file

---
+++ meetup-widgets-loomchild.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Meetup Widgets Loomchild
+ * Description: Widgets displaying events from Meetup.com
+ * Version: 1
+ * Author: Jarek Lipski
+ * Author URI: http://loomchild.net
+ * Plugin URI: http://loomchild.net
+ */
+
+
