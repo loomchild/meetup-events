d14e01ab9bf8ccc749617c1cb62a0f870f682186
Jarek Lipski <loomchild@loomchild.net>
2019-09-01 17:34:55 +0200

    comma

--- meetup-list.php
+++ meetup-list.php
@@ -4,7 +4,7 @@ global $events;
 echo '<ul class="meetup_list">';
 foreach ( $events as $event ) {
 	printf(
-		'<li><a href="%1$s">%2$s</a>; %3$s</li>',
+		'<li><a href="%1$s">%2$s</a>, %3$s</li>',
 		esc_url( $event->link ),
 		strip_tags( $event->name ),
 		date( 'M d, g:ia', intval( $event->time/1000 + $event->utc_offset/1000 ) )
